77e6c48be812f641ec017705f9b1e9d4d5833c8f
Bondarev Maxim <bonmax46@mail.ru>
2024-09-19 19:15:31 +0300

    change main.py

--- main.py
+++ main.py
@@ -1,39 +1,27 @@
+
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-import json
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
-# postgres_password = os.getenv("postgres_password")
-
 from models import create_tables, Publisher, Book, Stock, Shop, Sale
 
-DSN = "postgresql://postgres:postgres_password@localhost:5432/books"
+DSN = "postgresql://postgres:postgres@localhost:5432/books"
 engine = sqlalchemy.create_engine(DSN)
 
 create_tables(engine)
-
 Session = sessionmaker(bind=engine)
 session = Session()
 
-with open('tests_data.json', 'r') as fd:
-    data = json.load(fd)
+def getshops(author):
+    data = session.query(Book.title, Shop.name, Sale.count * Sale.price, Sale.date_sale).select_from(Shop).join(Stock).join(Book).join(Publisher).join(Sale)
 
-for record in data:
-    model = {
-        'publisher': Publisher,
-        'shop': Shop,
-        'book': Book,
-        'stock': Stock,
-        'sale': Sale,
-    }[record.get('model')]
-    session.add(model(id=record.get('pk'), **record.get('fields')))
-session.commit()
+    if author.isdigit():
+        result = data.filter(Publisher.id == author).all()
+    else:
+        result = data.filter(Publisher.name == author).all()
+    for title_book, name_shop, count_sale, date_sale in result:
+        print(f"{title_book: <40} | {name_shop: <10} | {count_sale: <8} | {date_sale.strftime('%d-%m-%Y')}")
 
-publisher_id = int(input("Введите ID издателя(автора): "))
-data = session.query(Book.title, Shop.name, Sale.count * Sale.price, Sale.date_sale).join(Publisher).join(Stock).join(Shop).join(Sale).filter(Publisher.id == publisher_id ).all()
-for book in data:
-    print(f"{book[0]} | {book[1]} | {book[2]} | {book[3]}")
+if __name__ == '__main__':
 
-session.close()
+    author = input("Введите имя или айди автора: ")
+    getshops(author)
 
